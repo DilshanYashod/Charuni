0349654ef3799c6c6c9391f23dc2f0c6d366587a
Infinityworl <138610383+Infinityworl@users.noreply.github.com>
2023-10-08 11:25:18 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Joelkb/DQ-the-file-donor.git /DQTheFileDonorBot
+  git clone https://github.com/Infinityworl/Charuni.git /Charuni
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /DQTheFileDonorBot
+  git clone $UPSTREAM_REPO /Charuni
 fi
-cd /DQTheFileDonorBot
+cd /Charuni
 pip3 install -U -r requirements.txt
 echo "Starting DQ-The-File-Donor...."
 python3 bot.py
