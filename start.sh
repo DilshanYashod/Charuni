700cce3353b78071e731fc6dcf76937d32ab32f0
Infinityworl <138610383+Infinityworl@users.noreply.github.com>
2023-10-13 19:44:08 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Infinityworl/Charuni.git /Charuni
+  git clone Infinityworl/Charuni /Charuni
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Charuni
