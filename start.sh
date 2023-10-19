4aabd66765894a6260cf5f8770be4b4c3fbcf133
AfsalHashim <138692053+CyberTG@users.noreply.github.com>
2023-10-19 20:13:17 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone Infinityworl/Charuni /Charuni
+  git clone https://github.com/Infinityworl/Charuni.git /Charuni
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Charuni
 fi
 cd /Charuni
 pip3 install -U -r requirements.txt
-echo "Starting charu-Lk-bot...."
+echo "𝙎𝙏𝘼𝙍𝙏𝙄𝙉𝙂..☀️"
 python3 bot.py
