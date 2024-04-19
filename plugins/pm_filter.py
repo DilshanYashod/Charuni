eacb1f151e8b2edede9d1606e07a11dc02b91ece
Infinityworl <138610383+Infinityworl@users.noreply.github.com>
2024-04-19 21:40:49 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -211,7 +211,7 @@ async def next_page(bot, query):
                     [InlineKeyboardButton("◄ ʙᴀᴄᴋ", callback_data=f"next_{req}_{key}_{off_set}"), InlineKeyboardButton(f"{math.ceil(int(offset)/int(MAX_B_TN))+1} / {math.ceil(total/int(MAX_B_TN))}", callback_data="pages")]
                 )
             elif off_set is None:
-                btn.append([InlineKeyboardButton("⭕ පිටු", callback_data="pages"), InlineKeyboardButton(f"{math.ceil(int(offset)/int(MAX_B_TN))+1} / {math.ceil(total/int(MAX_B_TN))}", callback_data="pages"), InlineKeyboardButton(""◄ ʙᴀᴄᴋ", callback_data=f"next_{req}_{key}_{n_offset}")])
+                btn.append([InlineKeyboardButton("⭕ පිටු", callback_data="pages"), InlineKeyboardButton(f"{math.ceil(int(offset)/int(MAX_B_TN))+1} / {math.ceil(total/int(MAX_B_TN))}", callback_data="pages"), InlineKeyboardButton("◄ ʙᴀᴄᴋ", callback_data=f"next_{req}_{key}_{n_offset}")])
             else:
                 btn.append(
                     [
