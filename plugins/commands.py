deb778497de88fba97cacb66b74d85aea8f7188d
Infinityworl <138610383+Infinityworl@users.noreply.github.com>
2024-04-22 00:00:13 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -20,6 +20,7 @@ BATCH_FILES = {}
 
 @Client.on_message(filters.command("start") & filters.incoming)
 async def start(client, message):
+    botid = client.me.id
     await message.react(emoji=random.choice(REACTIONS))
     if message.chat.type in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]:
         buttons = [[
