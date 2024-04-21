0be809ba507082e3892fccb19fcbc82a830790fe
Infinityworl <138610383+Infinityworl@users.noreply.github.com>
2024-04-21 10:15:31 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -328,18 +328,53 @@ async def restart_bot(bot, message):
         file.write(f"{msg.chat.id}\n{msg.id}")
     os.execl(sys.executable, sys.executable, "bot.py")
 
-slow_mode_duration = 60
+@Client.on_message(filters.command("groupinfo") & filters.user(ADMIMS))
+async def get_group_status(bot, message):
+    if len(message.command) != 2:
+        await message.reply("Please provide a group username. Example: `/groupinfo YourGroupUsername`")
+        return
+    
+    group_username = message.command[1]
+    
+    try:
+        group = await client.get_chat(group_username)
+    except Exception as e:
+        await message.reply(f"Error: {e}")
+        return
+    
+    total_members = await client.get_chat_members_count(group.id)
+    group_description = group.description
+    premium_acc = banned = deleted_acc = bot = 0  # You should replace these variables with actual counts.
 
-@Client.on_message(filters.command('slow') & filters.user(ADMINS))
-async def set_slow_mode(bot, message):
-msg = await message.reply("<b>Restarting successfully✅...</b>")   
-    chat_id,
-    user_id=None,  # To apply to all users
-    permissions={"can_send_messages": True},
-    until_date=None,
-    set_slow_mode=slow_mode_duration
+    response_text = (
+        f"➖➖➖➖➖➖➖\n"
+        f"➲ GROUP NAME : {group.title} ✅\n"
+        f"➲ GROUP ID : {group.id}\n"
+        f"➲ TOTAL MEMBERS : {total_members}\n"
+        f"➲ DESCRIPTION : {group_description or 'N/A'}\n"
+        f"➲ USERNAME : @{group_username}\n"
+       
+        f"➖➖➖➖➖➖➖"
+    )
+    
+    await message.reply(response_text)
 
 
+
+# Command handler to get group status
+@Client.on_message(filters.command("status") & filters.group)
+def group_status(client, message):
+    chat = message.chat  # Chat where the command was sent
+    status_text = f"Group ID: {chat.id}\n" \
+                  f"Title: {chat.title}\n" \
+                  f"Type: {chat.type}\n"
+                  
+    if chat.username:  # Not all groups have a username
+        status_text += f"Username: @{chat.username}"
+    else:
+        status_text += "Username: None"
+
+    message.reply_text(status_text)
 
 import os, asyncio
 from pyrogram import Client, filters
