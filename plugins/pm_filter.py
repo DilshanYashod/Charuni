9ab2157de228396b78ffcb64c4a29117f82a833c
Infinityworl <138610383+Infinityworl@users.noreply.github.com>
2024-04-24 20:06:09 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1708,7 +1708,7 @@ async def auto_filter(client, msg, spoll=False):
  ├ Tᴏᴛᴀʟ Usᴇʀs: <code>{search}</code>
  ├ Tᴏᴛᴀʟ Cʜᴀᴛs: <code>{total_results}</code>
  ├ Usᴇᴅ Sᴛᴏʀᴀɢᴇ: <code>{message.chat.title}</code>
- └Fʀᴇᴇ Sᴛᴏʀᴀɢᴇ: <code>{message.from_user.mention}</code></b>"""</b>"
+ └Fʀᴇᴇ Sᴛᴏʀᴀɢᴇ: <code>{message.from_user.mention}</code></b>"
     if imdb and imdb.get('poster'):
         try:
             hehe = await message.reply_photo(photo=imdb.get('poster'), caption=cap[:1024], reply_markup=InlineKeyboardMarkup(btn))
