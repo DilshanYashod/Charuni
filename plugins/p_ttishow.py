0702629638de9ef1c92fff285efcd793e55bf450
Infinityworl <138610383+Infinityworl@users.noreply.github.com>
2024-04-25 12:28:15 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -1,13 +1,13 @@
 import random, os, sys
 from pyrogram import Client, filters, enums
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery , ChatPermissions
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery 
 from pyrogram.errors.exceptions.bad_request_400 import MessageTooLong, PeerIdInvalid
 from info import ADMINS, LOG_CHANNEL, SUPPORT_CHAT, MELCOW_NEW_USERS, MELCOW_VID, CHNL_LNK, GRP_LNK
 from database.users_chats_db import db
 from database.ia_filterdb import Media
 from utils import get_size, temp, get_settings
 from Script import script
-from pyrogram.errors import ChatAdminRequired
+from pyrogram.errors import ChatAdminRequired, ChatPermissions
 import asyncio 
 
 """-----------------------------------------https://t.me/GetTGLink/4179 --------------------------------------"""
